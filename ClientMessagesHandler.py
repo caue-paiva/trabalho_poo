70ca1bc86609c8e6dfb9e21842f98af572b5fbce
caue <cauepaivalira@outlook.com>
2024-06-13 23:32:35 -0300

    sockets com várias thread funcionando e criando classe para conectar as mensagens da socket com a classe que lida do trabalho de arquivos

---
+++ ClientMessagesHandler.py
@@ -0,0 +1,47 @@
+from TrabalhoArquivosHandler import TrabalhoArquivosHandler
+
+
+__ArquivosHandler: TrabalhoArquivosHandler = TrabalhoArquivosHandler()
+
+class ClientMessagesHandler:
+
+   @staticmethod
+   def _split_messages(message:str)->dict:
+      list_of_params:list[str] = message.split(",")
+
+      return_dict:dict[str,str|int] = {}
+
+      for param in list_of_params:
+         field_val_separator_index:int = param.find(":") 
+         return_dict[param[:field_val_separator_index]] = param[field_val_separator_index+1:]
+
+      return return_dict 
+   
+   @staticmethod
+   def run_functionality(message:str, client_addr:int)->str:
+      funct_params:dict = ClientMessagesHandler._split_messages(message)
+
+      functionality:str = funct_params.get("functionality","")
+      if not functionality:
+         raise IOError("messagem do socket deve conter uma funcionalidade do trabalho de arquivos")
+
+      match (functionality):
+
+         case "1": #criar arquivo bin
+            pass
+         case "2":
+            pass
+         case "3":
+            pass
+
+
+   @staticmethod
+   def get_player_fields(message:str)->dict:
+       pass
+   
+   @staticmethod
+   def get_binary_file_names(message:str)->tuple[str]:
+       pass
+   
+
+print(ClientMessagesHandler._split_messages("funcionality:1,id:1298,age:32,country:GERMANY,club:asas,name:suhu"))
---
+++ Socketshandler.py
@@ -0,0 +1,71 @@
+import socket, threading , os
+
+class SocketsHandler():
+
+   PORT = 9090
+   MSG_BUFFER_SIZE = 1024
+   ENCODING = "utf-8"
+
+
+   server: socket.socket
+   local_ip:str
+   clients: dict
+
+   def __init__(self) -> None:
+      self.local_ip = self.__get_local_ip()
+      print(self.local_ip)
+
+      self.server = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #cria uma socket do tipo AF_INET (ipv4) e do tipo sock_stream (TCP)
+      self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #reusa a socket se ela já estiver aberta
+      self.server.bind((self.local_ip,self.PORT))
+      self.server.listen()
+
+      self.clients = {} #inicializa o dict de clientes como vazio
+
+   def __get_local_ip(self) -> str:
+         """
+         """
+         if os.name.lower() == "posix": #caso do linux
+            hostname = socket.gethostname()
+            local_ip = socket.gethostbyname(hostname) #esse método pode não funcionar no windows
+            return local_ip
+         else: #caso do windows
+               ip:str = ""
+               socket_temp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #cria uma socket temp para conectar a outro ip e pegar o ip local
+               try:
+                     socket_temp.connect(('10.254.254.254', 1))  #usa um outro IP sem possibilidade de rotear para achar o endereço de IP local
+                     ip = socket_temp.getsockname()[0]
+               except Exception:
+                     ip = '127.0.0.1' #caso padrão
+               finally:
+                     socket_temp.close()
+               return ip
+   
+   def __handle_one_client(self,client: socket.socket, addr:int)->None:
+      while True:
+         try:
+            message:str = client.recv(self.MSG_BUFFER_SIZE).decode(self.ENCODING)
+            print(f"recebeu mensagem : {message} do addr {addr}")
+         except:
+            removed_addr = self.clients.pop(client) #remove cliente
+            client.close()
+            print(f"endereço removido {str(removed_addr[1])}")
+
+   def receive_messages(self)->None:
+      print("server está ouvindo")
+      while True:
+         client,adress = self.server.accept() #aceita conexão com um cliente
+         print(f"conectou com endereço {str(adress[1])}")
+
+         self.clients[client] = adress #add cliente e seu endereço ao dict de clientes
+
+         thread = threading.Thread(target=self.__handle_one_client, args=(client,adress[1])) #cria um thread para cada cliente
+         thread.start() #executa a thread
+         print(self.clients_info())
+
+   def clients_info(self)->str:
+      return str([str(address[1]) for address in self.clients.values()])
+
+if __name__ == "__main__":
+   my_socket = SocketsHandler()
+   my_socket.receive_messages()
